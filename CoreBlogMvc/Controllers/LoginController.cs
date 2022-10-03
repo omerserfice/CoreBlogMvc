ee9cd4c88a447b0a34b2a35c2a24c6ca188ac873
Omer <srfcomr@gmail.com>
2022-10-03 22:52:51 +0300

    03.10.2022

--- CoreBlogMvc/Controllers/LoginController.cs
+++ CoreBlogMvc/Controllers/LoginController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace CoreBlogMvc.Controllers
 {
     public class LoginController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task< IActionResult> Index(Writer p)
+        {
+            Context c=new Context();
+            var datavalue= c.Writers.FirstOrDefault(x => x.WriterEmail == p.WriterEmail && x.WriterPassword == p.WriterPassword);
+            if (datavalue != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.WriterEmail)
+                };
+                var userIdentity = new ClaimsIdentity(claims, "a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal);
+
+                return RedirectToAction("Index", "Writer");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
+
+//Context c = new Context();
+//var datavalue = c.Writers.FirstOrDefault(x => x.WriterEmail == p.WriterEmail && x.WriterPassword == p.WriterPassword);
+//if (datavalue != null)
+//{
+//    HttpContext.Session.SetString("username", p.WriterEmail);
+//    return RedirectToAction("Index", "Writer");
+//}
+//else
+//{
+//    return View();
+//}
+
---
+++ CoreBlogMvc/Controllers/WriterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlogMvc.Controllers
+{
+    
+    public class WriterController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+    }
+}
